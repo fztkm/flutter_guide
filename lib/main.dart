6ca52f839faeab7a1f2e11dce79657c2225f875f
fztkm <forthune.0517@gmail.com>
2021-08-12 20:32:11 +0900

    終了したときの状態を追加

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class _MyAppState extends State<MyApp> {
     });
   }
 
-  var questions = [
+  final questions = const [
     {
       'questionText': 'What\'s your favorite color?',
       'answer': ['Black', 'White', 'Blue', 'Red'],
@@ -45,16 +45,20 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: Text('question'),
         ),
-        body: Column(
-          children: [
-            Question(
-              questions[_questionIndex]['questionText'].toString(),
-            ),
-            ...(questions[_questionIndex]['answer'] as List<String>)
-                .map((String answer) => Answer(answerQuestion, answer))
-                .toList(),
-          ],
-        ),
+        body: _questionIndex < questions.length
+            ? Column(
+                children: [
+                  Question(
+                    questions[_questionIndex]['questionText'].toString(),
+                  ),
+                  ...(questions[_questionIndex]['answer'] as List<String>)
+                      .map((String answer) => Answer(answerQuestion, answer))
+                      .toList(),
+                ],
+              )
+            : Center(
+                child: Text('You did it.'),
+              ),
       ),
     );
   }
